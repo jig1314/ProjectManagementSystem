a420177b68c940190d8213196a4c6b457a9c0e04
jig1314 <jig1314@gmail.com>
2020-07-12 13:42:39 -0400

    Last Minute UI/UX modifications plus added Abourt Section. Deployed.

--- ProjectManagementSystem/Controllers/HomeController.cs
+++ ProjectManagementSystem/Controllers/HomeController.cs
@@ -20,12 +20,12 @@ namespace ProjectManagementSystem.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
         public IActionResult Error()
         {
